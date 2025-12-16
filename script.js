1ca4a882afcee9725a2271a4d87e2110d78d0bc2
aboosam532002-source <aboosam532002@gmail.com>
2025-12-16 23:51:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-async function sendOTP() {
-  const phone = document.getElementById("phone").value;
-  const result = document.getElementById("result");
-
-  result.innerText = "Sending...";
-
-  try {
-    const res = await fetch(
-      "https://phone-verify-api-eight.vercel.app/api/send-code",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ phone })
-      }
-    );
-
-    const data = await res.json();
-
-    if (data.success) {
-      result.innerText = "OTP Sent ✅";
-    } else {
-      result.innerText = data.error || "Error ❌";
-    }
-  } catch (err) {
-    console.error(err);
-    result.innerText = "Server error ❌";
-  }
-}
+fetch("https://phone-verify-api-eight.vercel.app/api/send-code", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    phone: phone
+  })
+});
